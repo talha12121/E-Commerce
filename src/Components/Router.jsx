fa698e1c2f4e9a3af91c408f5c17b6b350639d30
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2023-10-07 21:03:39 +0500

    Talha (#4)
    * all produts done
    
    * add new pages
    
    * done
    
    * ..

--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./Signup/Signup";
 import Login from "./Login/Login";
 import Home from "./Home/Home";
+import Detail from "./Details/Detail";
+
 
 export default function RouterApp() {
   return (
@@ -12,7 +14,8 @@ export default function RouterApp() {
         <Route path="/Signup" element={<Signup />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route />
+      <Route path="/product/:productId" element={<Detail/>} />
+       
       </Routes>
     </BrowserRouter>
   );
