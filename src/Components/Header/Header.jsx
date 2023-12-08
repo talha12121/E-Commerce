df7aafe43bc6058f50c7e4e78d3ba01e30785045
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2023-12-09 04:15:18 +0500

    Talha (#11)
    * all produts done
    
    * add new pages
    
    * done
    
    * ..
    
    * docapp
    
    * authentication with security
    
    * firebase image upload done
    
    * .
    
    * changes
    
    * get current user
    
    * manage doc /pat

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Logo from "../../assests/docLogo.png"
 import "./Header.css";
 import { Link } from "react-router-dom";
-
+import DropDown from "./Dropdown";
+import Dashboard from "../Dashboard/Dashboard";
 export default function Header({links , text ,onClick}) {
-
+  
+  
   return (
     <>
+   
       <div className="header">
         <div className="header_logo">
           <img src={Logo} alt="" style={{mixBlendMode:"color-burn"}} width={"100px"} />
@@ -15,6 +18,7 @@ export default function Header({links , text ,onClick}) {
         
         <div className="signup_btn">
         <Link to={links} className="signup_btn_link"><button onClick={onClick} >{text}</button></Link>
+          <DropDown/>
         </div>
         
       </div>
