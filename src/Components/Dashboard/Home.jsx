7de660cfde4d9323b6e63ff7634181656ec0b384
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2024-01-04 02:53:38 +0500

    Talha (#14)
    * all produts done
    
    * add new pages
    
    * done
    
    * ..
    
    * docapp
    
    * authentication with security
    
    * firebase image upload done
    
    * .
    
    * changes
    
    * get current user
    
    * manage doc /pat
    
    * apply tailwind
    
    * hero section
    
    * middle

--- src/Components/Dashboard/Home.jsx
+++ src/Components/Dashboard/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import image from "../../assests/header-bg.jpg";
 import Hero from "./Hero";
+import MiddleSection from "./MiddleSection";
 import Header from "../Header/Header";
 
 function Home() {
   return (
     <>
       <Header />
-       <Hero />
+       <Hero /> 
+       <MiddleSection />
     </>
   );
 }
--- src/Components/Header/Dropdown.jsx
+++ src/Components/Header/Dropdown.jsx
@@ -6,19 +6,19 @@ import { useUserContext } from '../Redux/Context';
 import { FaUser } from "react-icons/fa6";
 const items = [
   {
-    label: <a href="">Profile</a>,
+    label: <a className="no-underline" href="">Profile</a>,
     key: '0',
   },
   {
-    label: <a href="">Logout</a>,
+    label: <a className="no-underline" href="">Logout</a>,
     key: '1',
   },
   {
-    label: <a href="/signup">Sign up</a>,
+    label: <a className="no-underline" href="/signup">Sign up</a>,
     key: '2',
   },
   {
-    label: <a href="/login">Login</a>,
+    label: <a className="no-underline" href="/login">Login</a>,
     key: '3',
   },
  
@@ -30,16 +30,15 @@ console.log(filteredUsers)
     return(
 
         <Dropdown
+        className="!w-[25%]"
         menu={{
             items,
         }}
         trigger={['click']}
         >
-    <a onClick={(e) => e.preventDefault()}>
+    <a  onClick={(e) => e.preventDefault()}>
       <Space className="cursor-pointer " >
       <FaUser color='black' size={20} className='cursor-pointer' />
-        {/* <img width={35} style={{borderRadius:"50%" , cursor:"pointer"}} src={defaultLogo} alt="" /> */}
-        
       </Space>
     </a>
   </Dropdown>
