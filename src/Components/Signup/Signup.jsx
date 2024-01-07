59d9285e4e4f34294aa2055474bac95100ba844c
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2024-01-08 01:27:31 +0500

    remove all css

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Logo from "../../assests/logo.png";
 import { Link } from "react-router-dom";
-import "./Signup.css";
 import { useState } from "react";
 import Swal from "sweetalert2";
 import { auth, storage } from "../../config.jsx";
