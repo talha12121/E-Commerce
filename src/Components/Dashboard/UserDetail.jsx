8b1ace156360c3d36c9c66efbbbd82a23b9bcd2e
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2024-02-04 03:08:35 +0500

    Talha (#23)
    * all produts done
    
    * add new pages
    
    * done
    
    * ..
    
    * docapp
    
    * authentication with security
    
    * firebase image upload done
    
    * .
    
    * changes
    
    * get current user
    
    * manage doc /pat
    
    * apply tailwind
    
    * hero section
    
    * middle
    
    * context done
    
    * remove all css files
    
    * remove all css
    
    * context done
    
    * developed UI
    
    * changes
    
    * changes
    
    * changes
    
    * appointment changes
    
    * Ui changes

--- src/Components/Dashboard/UserDetail.jsx
+++ src/Components/Dashboard/UserDetail.jsx
@@ -83,6 +83,7 @@ function UserDetail() {
 
           <div className="row">
             <div className="col-md-12 ">
+              
               <Card className="!rounded-3xl mt-2 !shadow-xl overflow-hidden">
                 <Card.Img
                   variant="top"
@@ -113,9 +114,146 @@ function UserDetail() {
                      <Button className="border w-50 pt-2 pb-2 " onClick={appointment}>Book An Appointment</Button>
                    : ""}
                    </div>
-              </Card>
+              </Card> 
+              
+            {/* </div>
+          </div>
+          {/* <div className="lg:p-4 !shadow-xl border border-zinc-200 rounded">
+          
+          <div className="row align-items-center p-2">
+            <div className="col-md-2 self-start">
+              
+              <div className="row ">
+                <div className="col-3 col-md-12 md:px-3">
+                  <div className="relative">
+                    <div className="relative border-0">
+                     <img src={userData.image ? userData.image : defaultLogo} className="!rounded-full !border !border-zinc-700 w-[100px] h-[100px]" />
+                     
+                    </div>
+                  </div>
+                </div>
+                <div className="col-9 pl-1  md:hidden">
+                  <h2>
+                    <div className="mb-1 mt-1">{userData.role === "doctor" ?"Dr. " + userData.name :userData.name} </div>
+                    </h2>
+                  <p className="mb-1">
+                    General Physician, Family Physician, Hypertension
+                    Specialist, Consultant Physician
+                  </p>
+                  <p className="mb-1 ">MBBS</p>
+                </div>
+              </div>
+              <div className="doc-discription-columns text-area d-md-none">
+                <div className="item">
+                  <span className="od-wte">10 Years</span>
+                  <span className="od-wte-text-muted">Experience</span>
+                </div>
+                <div className="item">
+                  <span className="od-wte">
+                    100%
+                    <span className="d-inline-block font-normal">
+                      (116)
+                    </span>
+                  </span>
+
+                  <span className="d-patient-none od-wte-text-muted">
+                    Satisfied
+                  </span>
+                  <span className="d-patient-none-2 od-wte-text-muted">
+                    Satisfied
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-10 col-lg-7 pt-md-0 px-0 pl-md-2 pr-md-4 mb-md-2 pb-md-2 mx-2 mx-md-0 listing-card-info">
+              <div className="d-none d-md-block text-area">
+                <h2>
+                  <div className="doctor-name mb-1 mt-2" style={{display:"flex" , alignItems:"center" ,fontWeight: 400}}>
+                    {userData.role === "doctor" ?"Dr. " + userData.name :userData.name} 
+                  </div>
+                </h2>
+                <p className="mb-1 doc-specialization">
+                  General Physician, Family Physician, Hypertension
+                  Specialist, Consultant Physician
+                </p>
+                <p className="mb-1  doc-degree">MBBS</p>
+                <div className="doc-discription-columns">
+                  <div className="item">
+                    <span className="font-medium">{userData.experience} Years </span>
+                    <span className="od-wte-text-muted">Experience</span>
+                  </div>
+                  <div className="item">
+                    <div>
+                      <span className="font-medium">
+                        100%
+                        <span className="inline-block">(116)</span>
+                      </span>
+                      <span className="od-wte-text-muted">
+                        Satisfied Patients
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 lg:mb-2 lg:pb-2 btn-holder order-3 lg:order-2 ">
+              <div className="row lg:pt-0">
+              
+                <div className="col-6 col-lg-12  mb-2">
+                  <div className="border border-[#001766] text-[#001766] p-2 flex items-center flex-wrap justify-center" >
+                    Video Consultation </div>
+                </div>
+               
+                <div className="col-6 col-lg-12 mb-2 ">
+                  
+                    <div className="bg-[#ff9e15] text-white p-2 flex items-center flex-wrap justify-center">
+
+                    Book Appointment
+                    </div>
+                  
+                </div>
+              </div>
+            </div>
+            
+            <div
+              className="col-12 pl-0 md:px-3 lg:py-2 lg:order-4"
+             
+            >
+             
+              <div >
+                <div>
+                  <div className="p-2 ">
+                    <div className="flex text-[14px] font-medium items-center">
+                    
+                      <span className="block mb-1 font-medium">
+                        Online Video Consultation (Online)
+                      </span>
+                      <span className="hidden ml-auto flex-shrink-0 pl-2">
+                        Rs. 1,000
+                      </span>
+                    </div>
+                    <div className="flex text-[11px]">
+                      <div className="px-2  font-medium">
+                        <div className="row items-center no-underline">
+                          <span className="mr-2 "></span>
+                          <span className="col pl-0  text-[14px] font-normal">
+                            Available tomorrow
+                          </span>
+                        </div>
+                      </div>
+                      <div className="inline-flex flex-shrink-0 ml-auto text-[14px] font-medium">
+                        <span className="doctor-fee">Rs. 1,000</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+        </div> */}
+      
+        </div>
+        </div>
         </div>
       )}
     </>
--- src/Components/Header/Profile.jsx
+++ src/Components/Header/Profile.jsx
@@ -1,37 +1,39 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Header from './Header';
-import Loader from '../Loader/Loader';
-import defaultLogo from '../../assests/default_img.png';
-import Card from 'react-bootstrap/Card';
-import NoteContext from '../Context/NoteContext';
-
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import Header from "./Header";
+import Loader from "../Loader/Loader";
+import defaultLogo from "../../assests/default_img.png";
+import Card from "react-bootstrap/Card";
+import NoteContext from "../Context/NoteContext";
 
 function Profile() {
   const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState(null);
-  const {userContext , setUserContext} = useContext(NoteContext)
-
+  const { userContext, setUserContext } = useContext(NoteContext);
+  const navigate = useNavigate();
   useEffect(() => {
-    
-    
-    const getCurrentUserData = localStorage.getItem('currentUserData');
-    const jsonCurrentUserData = JSON.parse(getCurrentUserData);
-    // if(jsonCurrentUserData.role === "doctor"){
+    const getToken = localStorage.getItem("token");
+    if (getToken) {
+      const getCurrentUserData = localStorage.getItem("currentUserData");
+      const jsonCurrentUserData = JSON.parse(getCurrentUserData);
+      // if(jsonCurrentUserData.role === "doctor"){
 
       // const appointmentKeys = Object.keys(userContext.appointments);
       // appointmentKeys.forEach(key => {
       //   const appointmentData = userContext.appointments[key];
       //   console.log(`Data for appointment ${key}:`, appointmentData);
       // });
-    // }
-    if (getCurrentUserData) {
+      // }
+      if (getCurrentUserData) {
         setUserData(jsonCurrentUserData);
       } else {
-        console.error('Error parsing user data:');
-        
+        console.error("Error parsing user data:");
       }
       setLoading(false);
-    }, []);
+    } else {
+      navigate("/login");
+    }
+  }, []);
 
   return (
     <>
@@ -48,37 +50,48 @@ function Profile() {
                 <Card.Img
                   variant="top"
                   className="mr-auto ml-auto pt-2 sm:!w-52 h-52 lg:!w-52 md:!w-52"
-                  src={userData && userData.image ? userData.image : defaultLogo}
+                  src={
+                    userData && userData.image ? userData.image : defaultLogo
+                  }
                 />
 
                 <Card.Body>
                   <Card.Title className="!text-[15px]">
-                    Name = {userData && userData.name ? userData.name : 'NIL'}
+                    Name = {userData && userData.name ? userData.name : "NIL"}
                   </Card.Title>
                   <Card.Title className="!text-[15px]">
-                    Email = {userData && userData.email ? userData.email : 'NIL'}
+                    Email =
+                    {userData && userData.email ? userData.email : "NIL"}
                   </Card.Title>
                   <Card.Title className="!text-[15px]">
-                    Gender = {userData && userData.gender ? userData.gender : 'NIL'}
+                    Gender =
+                    {userData && userData.gender ? userData.gender : "NIL"}
                   </Card.Title>
                   {userData.age && (
-                  <Card.Title className="!text-[15px]">
-                    Age = {userData && userData.age ? userData.age + ' year' : 'NIL'}
-                  </Card.Title>
+                    <Card.Title className="!text-[15px]">
+                      Age =
+                      {userData && userData.age
+                        ? userData.age + " year"
+                        : "NIL"}
+                    </Card.Title>
                   )}
                   {userData.experience && (
-                  <Card.Title className="!text-[15px]">
-                    Experience ={userData && userData.experience? userData.experience + ' year': 'NIL'}
-                  </Card.Title>
+                    <Card.Title className="!text-[15px]">
+                      Experience =
+                      {userData && userData.experience
+                        ? userData.experience + " year"
+                        : "NIL"}
+                    </Card.Title>
                   )}
-                  {/* <Card.Title className="!text-[15px]">
+                   {/* <Card.Title className="!text-[15px]">
                      {userData.role === "doctor" ?   userContext ?"Appointment By =" + userContext.appointments.data.name:"Appointment By =" : ""}
-                  </Card.Title> */}
-                </Card.Body>
-              </Card>
-            </div>
-          </div>
-        </div>
+                  </Card.Title>  */}
+                 </Card.Body>
+              </Card> 
+              </div> 
+              </div> 
+              </div> 
+
       )}
     </>
   );
